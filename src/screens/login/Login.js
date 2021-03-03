8ba6a84277314f0618a3cbea63a7cf2bc90461ee
Sarjith <rmsarju@gmail.com>
2021-03-04 00:34:39 +0530

    Login Header

---
+++ src/screens/login/Login.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './Login.css';
+
+class Login extends Component{
+    render(){
+        return(
+            <div>
+                <header className="app-header">
+                    <p>Image Viewer</p>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Login;
